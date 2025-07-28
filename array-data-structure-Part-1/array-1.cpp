4b55493ad0f3adbee46460c71ccd86ad1b9668a7
chandu8861 <chandus10906@gmail.com>
2025-07-28 17:41:56 +0530

    Intialization of an array

---
+++ array-data-structure-Part-1/array-1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int marks[5] = {10, 20, 30, 40, 50}; // initialization of array containing five elements
+
+    cout << marks[0] << endl; 
+    cout << marks[1] << endl;
+    cout << marks[2] << endl;
+    cout << marks[3] << endl; 
+    cout << marks[4] << endl;
+    
+    /* Output :
+    10
+    20
+    30
+    40
+    50
+    */
+    return 0;
+}
