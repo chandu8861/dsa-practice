bc483eb1b35a9b3ec01f4f04e25cf891787ede9e
chandu8861 <chandus10906@gmail.com>
2025-08-21 09:32:08 +0530

    add palindrome checker with alphanumeric filtering

--- strings2-problems/valid-palindrome.cpp
+++ strings2-problems/valid-palindrome.cpp
@@ -60,3 +60,18 @@ int main (){
 
 // Enter a string to check whether it is palindrome or not : ""
 // Palindrome
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int start = 0, end = s.size()-1;
+        while(start < end){
+            if (!isalnum(s[start])) {start++;continue;}
+            if (!isalnum(s[end])) {end--;continue;}
+            if (tolower(s[start]) != tolower(s[end])) return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+};
