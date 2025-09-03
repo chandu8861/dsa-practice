11e4c169ddbe26c26ff5a3ece4059b85847470e2
chandu8861 <chandus10906@gmail.com>
2025-09-03 10:46:25 +0530

    implement corrected version of recursive binary search

--- recursion/binary-search.cpp
+++ recursion/binary-search.cpp
@@ -8,11 +8,11 @@ using namespace std;
 
 // Helper function :
 int binarySearch (vector<int> arr, int target, int start, int end){
+    if (start > end) return -1;
     int mid = start + (end - start)/2;
     if (arr[mid] == target) return mid;
     else if (arr[mid] > target) return binarySearch (arr, target, start, mid-1);
     else  return binarySearch (arr, target, mid+1, end);
-    return -1;
 }
 
 // Given function :
@@ -26,3 +26,20 @@ int main(){
     return 0;
 }
 // 4
+
+// class Solution {
+// public:
+//     int binarySearch (vector<int> nums, int target, int start, int end){
+//         if (start <= end){
+//             int mid = start + (end - start)/2;
+//             if (nums[mid] == target) return mid;
+//             else if (nums[mid] > target) return binarySearch (nums, target, start, mid-1);
+//             else  return binarySearch (nums, target, mid+1, end);
+//         }
+//         return -1;
+//     }
+
+//     int search(vector<int>& nums, int target) {
+//         return binarySearch (nums, target, 0, nums.size()-1);
+//     }
+// };
